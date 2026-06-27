85325a71e21441dd957817c5dd08009d218e4598
olenevoid <mazekunov@gmail.com>
2026-06-27 15:48:35 +0500

    Добавил поддержку прокси для бота

--- metup_bot/management/commands/runbot.py
+++ metup_bot/management/commands/runbot.py
@@ -2,13 +2,17 @@ from django.core.management.base import BaseCommand
 from telegram.ext import ApplicationBuilder
 
 from tg_bot.handlers.state_machines import get_root_conversation_handler
-from tg_bot.settings import TG_BOT_TOKEN
+from tg_bot.settings import BOT_PROXY, TG_BOT_TOKEN
 
 
 class Command(BaseCommand):
     help = "Run the Telegram bot in polling mode."
 
     def handle(self, *args, **options):
-        application = ApplicationBuilder().token(TG_BOT_TOKEN).build()
+        builder = ApplicationBuilder().token(TG_BOT_TOKEN)
+        if BOT_PROXY:
+            builder = builder.proxy(BOT_PROXY)
+            builder = builder.get_updates_proxy(BOT_PROXY)
+        application = builder.build()
         application.add_handler(get_root_conversation_handler())
         application.run_polling()
--- tg_bot/settings.py
+++ tg_bot/settings.py
@@ -4,5 +4,6 @@ env = Env()
 env.read_env()
 
 TG_BOT_TOKEN = env.str("TG_BOT_TOKEN")
+BOT_PROXY = env.str("BOT_PROXY", "")
 YOOKASSA_SHOP_ID = env.str("YOOKASSA_SHOP_ID", "")
 YOOKASSA_SECRET_KEY = env.str("YOOKASSA_SECRET_KEY", "")
