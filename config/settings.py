82a92debb4a174fa1086222f9c0d23edc1e190b3
olenevoid <mazekunov@gmail.com>
2026-06-30 20:42:37 +0500

    Добавил CSRF источники в настройки

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 DEBUG = True
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", [])
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", [])
 
 
 # Application definition
